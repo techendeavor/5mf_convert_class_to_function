1f8dea6d1e6b83f5fbf074e3bd71233cc35b07ee
Clif Hodges <chodges@Clifs-MBP.austin.rr.com>
2020-12-21 19:49:05 -0600

    complete tutorial

--- src/components/Todos/TodoForm.js
+++ src/components/Todos/TodoForm.js
@@ -1,39 +1,94 @@
-import React, { useState } from 'react'
+import React, { Component } from 'react'
 
-export default function TodoForm({ todos, setTodos  }) {
-
-    const initialState = {
-        id: '',
-        message: ''
+export default class TodoForm extends Component {
+    constructor(props) {
+        super(props)
+        this.initialState = {
+            id: '',
+            message: ''
+        }
+        this.state = this.initialState
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
 
-    const [todo, setTodo] = useState(initialState)
-
-    const handleChange = e => {
-        setTodo({
+    handleChange(e) {
+        this.setState({
             id: Date.now(),
             message: e.target.value
         })
     }
 
-    const handleSubmit = e => {
+    handleSubmit(e) {
         e.preventDefault()
-        setTodos([ todo, ...todos ])
-        setTodo(initialState)
+        this.props.setTodos([ this.state, ...this.props.todos ])
+        this.setState(this.initialState)
     }
 
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    name="todo"
-                    value={todo.message}
-                    placeholder="Enter your Todo item"
-                    onChange={handleChange}
-                />
-                <button type="submit">Add Todo</button>
-            </form>
-        </div>
-    )
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        type="text"
+                        name="todo"
+                        value={this.state.message}
+                        placeholder="Enter your Todo item"
+                        onChange={this.handleChange}
+                    />
+                    <button type="submit">Add Todo</button>
+                </form>
+            </div>
+        )
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState } from 'react'
+
+// export default function TodoForm({ todos, setTodos  }) {
+
+//     const initialState = {
+//         id: '',
+//         message: ''
+//     }
+
+//     const [todo, setTodo] = useState(initialState)
+
+//     const handleChange = e => {
+//         setTodo({
+//             id: Date.now(),
+//             message: e.target.value
+//         })
+//     }
+
+//     const handleSubmit = e => {
+//         e.preventDefault()
+//         setTodos([ todo, ...todos ])
+//         setTodo(initialState)
+//     }
+
+//     return (
+//         <div>
+//             <form onSubmit={handleSubmit}>
+//                 <input
+//                     type="text"
+//                     name="todo"
+//                     value={todo.message}
+//                     placeholder="Enter your Todo item"
+//                     onChange={handleChange}
+//                 />
+//                 <button type="submit">Add Todo</button>
+//             </form>
+//         </div>
+//     )
+// }
